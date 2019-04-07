e2225e76ec854b290acb0eeaedb8f56b81d865fc
John Bretherton <270022735@bun2-206-10.local>
2019-04-08 11:53:15 +1200

    Nav for mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topNav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topNav";
-  }
+//MAKING TOPNAV RESPONSIVE 
+// function myFunction() {
+//   var x = document.getElementById("hamburgerIcon");
+//   if (x.className === "topNav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topNav";
+//   }
+// }
+
+
+// function myFunction() {
+//   var x = document.getElementById("hamburgerIcon");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topNav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topNav";
+//   }
+// }
+
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+function showNav(){
+    var menu = document.getElementById("topNav");
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
-</script>
+
+
