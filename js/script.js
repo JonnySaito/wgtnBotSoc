6e0bde0822e40d926d8de7042efaa243c469e21f
John Bretherton <270022735@bun2-206-10.local>
2019-04-09 11:51:16 +1200

    More effort on Nav Bar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 //FUNCTION FOR DROP-DOWN MENU
-$("#myTopnav").hide();
+// $("#myTopnav").hide();
 $(document).ready(function(){
   $("#hamburgerIcon").click(function(){
     $("#myTopnav").toggle();
   });
 });
 
-/*$("#myTopnav").hide();
-function myFunction(){
-    var menu = document.getElementById("myTopnav");
-    if(menu.style.display === "none"){
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
-*/
+// $("#myTopnav").hide();
+// function myFunction(){
+//     var menu = document.getElementById("myTopnav");
+//     if(menu.style.display === "none"){
+//         menu.style.display = "block";
+//     } else {
+//         menu.style.display = "none";
+//     }
+// }
+
 
