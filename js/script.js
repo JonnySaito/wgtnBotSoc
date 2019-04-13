e4c42994286b16fbfc813b90791673f5f664a386
John Bretherton <270022735@bun2-206-10.local>
2019-04-13 19:15:46 +1200

    Home page and 3 other pages finished

--- js/script.js
+++ js/script.js
@@ -6,14 +6,6 @@ $(document).ready(function(){
   });
 });
 
-// $("#myTopnav").hide();
-// function myFunction(){
-//     var menu = document.getElementById("myTopnav");
-//     if(menu.style.display === "none"){
-//         menu.style.display = "block";
-//     } else {
-//         menu.style.display = "none";
-//     }
-// }
+
 
 
